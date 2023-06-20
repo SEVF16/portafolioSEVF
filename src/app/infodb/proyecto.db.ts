9f2fcaf144e604dc60c55a94f855358d918a8771
Shiryu Valecillos Personal <shiryuevfr@gmail.com>
2023-06-20 16:50:24 -0400

    se modifica path de img para deploy

--- src/app/infodb/proyecto.db.ts
+++ src/app/infodb/proyecto.db.ts
@@ -4,21 +4,21 @@ export const proyectoPe: Proyecto[] = [
   {
     id: 1,
     name: 'Rick&Morty',
-    img: '../assets/img/rickmorty.png',
+    img: './assets/img/rickmorty.png',
   },
   {
     id: 2,
     name: 'The Movie Web',
-    img: '../assets/img/themovieweb.png',
+    img: './assets/img/themovieweb.png',
   },
   {
     id: 3,
     name: 'Dummy',
-    img: '../assets/img/dummy.png',
+    img: './assets/img/dummy.png',
   },
   {
     id: 4,
     name: 'Ecommerce Geek',
-    img: '../assets/img/ecommerceAlura.png',
+    img: './assets/img/ecommerceAlura.png',
   },
 ]
